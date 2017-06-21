126235c93c694925f53962b0683c5ad0d2b1c15b
Gerrald Mitchell <bluelionjr@gmail.com>
2017-06-21 13:14:31 -0400

    User Sign Up

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,8 @@
+class UserMailer < ApplicationMailer
+  default from: "bluelionjr@gmail.com"
+
+  def new_user(user)
+    @user = user
+    mail(to: user.email, subject: "Welcome to Blocipedia, #{user}!")
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'shielded-basin-38159.herokuapp.com' }
 end
