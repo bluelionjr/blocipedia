9ddcbc07a767172486f9369cf128de7885aecb19
Gerrald Mitchell <bluelionjr@gmail.com>
2017-06-22 14:22:52 -0400

    Fixed Mailer, moved user info to navbar

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
   default from: "bluelionjr@gmail.com"
 
+
   def new_user(user)
     @user = user
-    mail(to: user.email, subject: "Welcome to Blocipedia, #{user}!")
+    mail(to: user.email, subject: "Welcome to Blocipedia!")
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :new_user
+
+  def new_user
+    UserMailer.new_user(self).deliver_now
+  end
 end
